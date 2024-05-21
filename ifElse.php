94cf2c81f290b916c5c3ffdcda265443713bd6e5
Oh seung min <ohmin@gmail.com>
2024-05-21 20:02:56 +0900

    php 기초 문법 조건문 작성 같은 값 크거나 작거나 비교 가능

---
+++ ifElse.php
@@ -0,0 +1,11 @@
+<?php
+$num = 15;
+
+if ($num > 10) {
+    echo "$num is the big number";
+} elseif ($num == 10) {
+    echo "the number is 10";
+} else {
+    echo "$num is the small number";
+}
+?>
